5ee5c09927ea0f314c1605d7e064836190129a5a
etwillbefine <tim.gatzemeier@gmx.de>
2019-09-01 15:04:28 +0200

    add read access to policies if logged in with mfa

--- policy.tf
+++ policy.tf
@@ -37,6 +37,9 @@ data "aws_iam_policy_document" "with_mfa" {
     effect = "Allow"
     actions = [
       "iam:GetUser",
+      "iam:ListGroupsForUser",
+      "iam:ListAttachedUserPolicies",
+      "iam:ListUserPolicies",
       "iam:GetAccountSummary",
       "iam:DeactivateMFADevice",
       "iam:ChangePassword",
@@ -56,6 +59,33 @@ data "aws_iam_policy_document" "with_mfa" {
       format("arn:aws:iam::%s:mfa/&{aws:username}", data.aws_caller_identity.current.account_id),
       format("arn:aws:iam::%s:user/&{aws:username}", data.aws_caller_identity.current.account_id),
       format("arn:aws:iam::%s:user/%s/&{aws:username}", data.aws_caller_identity.current.account_id, var.users_path),
+    ]
+
+    condition {
+      values   = ["true"]
+      test     = "BoolIfExists"
+      variable = "aws:MultiFactorAuthPresent"
+    }
+  }
+
+  statement {
+    effect  = "Allow"
+    actions = [
+      "iam:ListGroups",
+      "iam:ListRoles",
+      "iam:ListPolicies",
+      "iam:ListRolePolicies",
+      "iam:ListGroupPolicies",
+      "iam:ListAttachedRolePolicies",
+      "iam:ListAttachedGroupPolicies",
+    ]
+    resources = [
+      format("arn:aws:iam::%s:group/", data.aws_caller_identity.current.account_id),
+      format("arn:aws:iam::%s:group/*", data.aws_caller_identity.current.account_id),
+      format("arn:aws:iam::%s:policy/", data.aws_caller_identity.current.account_id),
+      format("arn:aws:iam::%s:policy/*", data.aws_caller_identity.current.account_id),
+      format("arn:aws:iam::%s:role/", data.aws_caller_identity.current.account_id),
+      format("arn:aws:iam::%s:role/*", data.aws_caller_identity.current.account_id),
     ]
 
     condition {
